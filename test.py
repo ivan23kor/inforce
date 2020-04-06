311cbc88ed4aaf046c6b45e43e1fb2d48ed4b646
ivan23kor <korostel@ualberta.ca>
2020-04-06 23:37:36 +0000

    Separating test and plot

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-
 from control import ROMC
 from env import RandomWalk, STANDARD_RANDOM_WALK
 from policy import Greedy, EpsilonGreedy, Random
@@ -15,21 +13,10 @@ if __name__ == "__main__":
     p, b_epsilon_greedy, b_random = EpsilonGreedy(0.2), EpsilonGreedy(0.5), Random()
     # Init control
     control = ROMC(env)
-    # control.generate_episode(b_greedy)
-    # exit(0)
 
     # Train and plot
-    # for gamma, marker in zip([0.9, 0.7, 0.4, 0.1], ["x", "o", ".", "v"]):
-    algorithm = "Weighted importance sampling (analytic)"
     algorithm = "Weighted importance sampling"
-    for gamma, marker in zip([0.1], ["x"]):
-        control.gamma = gamma
-        plt.plot(control.eval(p, b_epsilon_greedy, num_avg=2, num_episodes=50000,
-                              algorithm=algorithm),
-                 label="gamma = {}".format(gamma), marker=marker, markersize=4)
-    plt.title("Monte Carlo off-policy control\n {}".format(algorithm),
-              fontsize=23)
-    plt.xlabel("Episode number", fontsize=15)
-    plt.ylabel("Mean squared error, Q vs Q*", fontsize=15)
-    plt.legend(fontsize=20, loc="upper right")
-    plt.show()
+    with open("log", "w") as f:
+        res = control.eval(p, b_epsilon_greedy, num_avg=2,
+                           num_episodes=1000, algorithm=algorithm)
+        f.write(str(res))
